75f8f991849a76f11c77a7725c277b1c9e9a4d75
Khristina <hristinakirillova2559@gmail.com>
2023-11-13 14:51:28 +0300

    убраны коммы в index.js

--- public/index.js
+++ public/index.js
@@ -1,34 +1,4 @@
 import './index.css';
-// import MainPage from './src/pages/main-page/main-page.js';
-// import LoginPage from './src/pages/login-page/login-page.js';
-// import SignupPage from './src/pages/signup-page/signup-page.js';
-// import {loginROUTE, mainROUTE, signupROUTE} from './src/config/urls';
-// import router from './src/modules/router';
-// import {UserActions} from './src/actions/user';
-// import {eventEmmiter} from './src/modules/event-emmiter';
-// import {Events} from './src/config/events';
-
-// /**
-//  * Listener для нажатий по ссылкам
-//  * @param {Event} event Событие нажатия по ссылке
-//  */
-// const listenClick = (event) => {
-//     event.preventDefault();
-//     const anchor = event.target.closest('a');
-//     if (!anchor) return;
-//     router.go({url: anchor.getAttribute('href')});
-// };
-
-// document.addEventListener('DOMContentLoaded', UserActions.checkSession());
-// eventEmmiter.subscribe(Events.USER_IS_AUTH, router.go.bind(router));
-// eventEmmiter.subscribe(Events.USER_IS_NOT_AUTH, router.go.bind(router));
-// eventEmmiter.subscribe(Events.LOGOUT, router.go.bind(router));
-// const root = document.getElementById('root');
-// router.register({view: MainPage, url: mainROUTE, name: 'main'});
-// router.register({view: LoginPage, url: loginROUTE, name: 'login'});
-// router.register({view: SignupPage, url: signupROUTE, name: 'signup'});
-// router.start(root);
-// window.addEventListener('click', listenClick);
 
 import router from './src/modules/router';
 import {UserActions} from './src/actions/user';
@@ -41,7 +11,6 @@ const cstore = cartStore;
 const pstore = productsStore;
 document.addEventListener('DOMContentLoaded', UserActions.checkSession());
 eventEmmiter.subscribe(Events.USER_IS_AUTH, router.go.bind(router));
-// eventEmmiter.subscribe(Events.USER_IS_NOT_AUTH, router.go.bind(router));
 eventEmmiter.subscribe(Events.LOGOUT, router.go.bind(router));
 const root = document.getElementById('root');
 router.start(root);
