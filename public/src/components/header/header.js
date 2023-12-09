23ce92e53ac072eadce1b70d1b5da2f1e404fa1e
Ruslan Khalikov <rus-h-903@mail.ru>
2023-12-09 17:52:40 +0300

    fix remove logout btn

--- public/src/components/header/header.js
+++ public/src/components/header/header.js
@@ -125,7 +125,7 @@ export default class Header {
      * Удаление кнопки выхода
      */
     removeLogoutButton() {
-        if (this.logoutButton.self) {
+        if (this.logoutButton && this.logoutButton.self) {
             this.logoutButton.self.removeEventListener('click', this.logout);
             this.logoutButton.self.remove();
         }
