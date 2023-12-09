ec6907f1bb9dd442a50d333c57faea8111834bf3
Ruslan Khalikov <rus-h-903@mail.ru>
2023-12-09 19:27:26 +0300

    sw

--- public/sw.js
+++ public/sw.js
@@ -45,7 +45,7 @@ const update = (request, response) =>
 
 self.addEventListener('fetch', (event) => {
     console.log(event.request.headers);
-    if (event.request.headers['Content-type'].includes('image')) {
+    if (event.request.headers.get['Content-type'].includes('image')) {
         event.respondWith(
             fromCache(event.request).catch(() => fromNetwork(event.request, 2000)),
         );
