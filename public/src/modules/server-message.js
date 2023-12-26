2385b6560107779a453750c502799c0b7cbafda3
Ruslan Khalikov <rus-h-903@mail.ru>
2023-12-27 01:28:47 +0300

    fix message

--- public/src/modules/server-message.js
+++ public/src/modules/server-message.js
@@ -1,10 +1,24 @@
+export const INFO_MESSAGE = 'INFO_MESSAGE';
+
 /**
  * Отрисовка сообщений
  * @param {String} msg сообщение
- *  @param {Boolean} isCorrect Флаг для определния типа сообщения
+ *  @param {Boolean} type Флаг для определния типа сообщения
  */
-export function renderServerMessage(msg, isCorrect) {
-    const msgClass = isCorrect ? 'server-msg' : 'server-msg server-error';
+export function renderServerMessage(msg, type) {
+    let msgClass;
+    console.log(type);
+    switch (type) {
+    case undefined:
+        msgClass = 'server-msg server-error';
+        break;
+    case true:
+        msgClass = 'server-msg';
+        break;
+    case INFO_MESSAGE:
+        msgClass = 'server-msg info-msg';
+        break;
+    }
     const serverMsg = document.createElement('div');
     serverMsg.setAttribute('class', msgClass);
     serverMsg.textContent = msg;
