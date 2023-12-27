17d0c0bbab4557d655d7d5ae92054f763339225b
fitzplsr <90151369+fitzplsr@users.noreply.github.com>
2023-12-27 23:26:02 +0300

    Order status (#22)
    * add update orders
    
    * fix to create order
    
    * jsdoc

--- public/src/pages/orders-page/orders-page.js
+++ public/src/pages/orders-page/orders-page.js
@@ -79,6 +79,7 @@ export default class OrdersPage {
         if (!body) {
             return;
         }
+        this.self.querySelector('.orders-container').innerHTML = '';
         body.forEach((element) => {
             const product = new OrderItem(this.self.querySelector('.orders-container'),
                 this.getConfig(element));
@@ -98,6 +99,17 @@ export default class OrdersPage {
     }
 
     /**
+     * Обновление заказов
+     * @param {String} message
+     */
+    updateOrders(message) {
+        if (message.Type === 'profileRegistration') {
+            return;
+        }
+        CartActions.getAllOrders();
+    }
+
+    /**
      * Перенаправление на страницу авторизации
      */
     redirectToLogin() {
@@ -110,6 +122,7 @@ export default class OrdersPage {
     renderOrders = this.renderOrders.bind(this);
     renderAll = this.renderAll.bind(this);
     noOrders = this.noOrders.bind(this);
+    updateOrders = this.updateOrders.bind(this);
 
     /**
      * подписка на события
@@ -119,6 +132,7 @@ export default class OrdersPage {
         eventEmmiter.subscribe(Events.PAGE_FORBIDDEN, this.redirectToLogin);
         eventEmmiter.subscribe(Events.PAGE_ALLOWED, this.renderAll);
         eventEmmiter.subscribe(Events.NOT_FOUND, this.noOrders);
+        eventEmmiter.subscribe(Events.RECIEVE_NOTIFICATION, this.updateOrders);
     }
 
     /**
@@ -129,6 +143,7 @@ export default class OrdersPage {
         eventEmmiter.unsubscribe(Events.PAGE_FORBIDDEN, this.redirectToLogin);
         eventEmmiter.unsubscribe(Events.NOT_FOUND, this.noOrders);
         eventEmmiter.unsubscribe(Events.PAGE_ALLOWED, this.renderAll);
+        eventEmmiter.unsubscribe(Events.RECIEVE_NOTIFICATION, this.updateOrders);
     }
 
     /**
